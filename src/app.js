be30cc29736a246c38d24e2e96c685f5c0d27d2c
justinedaletanedo <justinedale.tanedo@gmail.com>
2022-07-28 16:57:16 +0100

    Changed the layout
    Removed popular cities, changed Spotify playlist and added new headline under search bar

--- src/app.js
+++ src/app.js
@@ -81,6 +81,8 @@ function showWeatherInfo(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#city-result").innerHTML = response.data.name;
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -102,48 +104,8 @@ function handleSubmit(event) {
   let city = document.querySelector("#input-city").value;
   search(city);
 }
-function updateMilan(event) {
-  event.preventDefault();
-  let city = "Milan";
-  let apiKey = "d97c0a348f5d52e2d0c7313a7a026be8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeatherInfo);
-}
-function updateIstanbul(event) {
-  event.preventDefault();
-  let city = "Istanbul";
-  let apiKey = "d97c0a348f5d52e2d0c7313a7a026be8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeatherInfo);
-}
-function updateNewYork(event) {
-  event.preventDefault();
-  let city = "New York";
-  let apiKey = "d97c0a348f5d52e2d0c7313a7a026be8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeatherInfo);
-}
-function updateLondon(event) {
-  event.preventDefault();
-  let city = "London";
-  let apiKey = "d97c0a348f5d52e2d0c7313a7a026be8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeatherInfo);
-}
 
 let submit = document.querySelector("#search-form");
 submit.addEventListener("submit", handleSubmit);
-
-let milanLink = document.querySelector("#milan-city");
-milanLink.addEventListener("click", updateMilan);
-
-let istanbulLink = document.querySelector("#istanbul-city");
-istanbulLink.addEventListener("click", updateIstanbul);
-
-let newyorkLink = document.querySelector("#newyork-city");
-newyorkLink.addEventListener("click", updateNewYork);
-
-let londonLink = document.querySelector("#london-city");
-londonLink.addEventListener("click", updateLondon);
 
 search("Bologna");
